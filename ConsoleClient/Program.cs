740c6648720faec117dcd860f3274330e9af63b4
Roman Bukin <artdev@icloud.com>
2013-12-01 23:59:18 +0400

    Full Commit

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EncryptedTcp;
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client("127.0.0.1", 10001);
+            client.SendText("Hello!");
+
+            //client.Connect();
+            //string serverMessage = client.ReceiveText();
+            //Console.WriteLine(serverMessage);
+            //client.SendText("I'am your client!");
+            Console.WriteLine("Ok!");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleServer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EncryptedTcp;
+using System.Net.Sockets;
+using EncryptedTcp.Processing;
+namespace ConsoleServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyServer srv = new MyServer();
+            srv.Port = 10001;
+            srv.Start();
+        }
+    }
+    class MyServer:Server
+    {
+        public override void ClientThread(object clientSocket)
+        {
+            TcpClient client = (TcpClient)clientSocket;
+            ServerProcessingEncrypted clientProc = new ServerProcessingEncrypted(client);
+            Console.WriteLine("Client: {0}", clientProc.ReceiveText());
+        }
+    }
+}
